242b015014da7a0ffd2d8b3e77e32df38c2dc537
vmoens <vincentmoens@gmail.com>
2023-11-16 17:08:29 +0000

    edits

--- torch/_functorch/eager_transforms.py
+++ torch/_functorch/eager_transforms.py
@@ -1370,7 +1370,7 @@ def grad_and_value_impl(func, argnums, has_aux, args, kwargs) -> Callable:
                     )
                 output, aux = output
 
-            if not isinstance(output, torch.Tensor):
+            if not isinstance(output, (torch.Tensor, torch.dict.TensorDictBase)):
                 raise RuntimeError(
                     "grad_and_value(f)(*args): Expected f(*args) "
                     f"to return a Tensor, got {type(output)}"
